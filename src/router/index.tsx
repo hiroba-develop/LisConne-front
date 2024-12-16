26234678eec6010d53c429582d1238f86b7eadca
shunsuke-nihei1931 <shunsuke-nihei@hiroba1931.co.jp>
2024-12-17 02:51:43 +0900

    add/企業リスト作成画面大枠

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "../pages/Home/Home";
 import Login from "../pages/Login/Login";
+import CreateCorporateList from "../pages/CreateCorporateList/CreateCorporateList";
 
 const AppRouter = () => {
   return (
@@ -8,6 +9,7 @@ const AppRouter = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/corporate-list/create" element={<CreateCorporateList />} />
       </Routes>
     </Router>
   );
